83439ffde7dad2e91b86f7d1ac330f6b351551d6
Aditya Kumar <69427863+Aditya-kumar121001@users.noreply.github.com>
2021-03-26 11:13:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-const city = document.querySelector('h1')
-const temp = document.querySelector('h3')
+
+const temp = document.querySelector('.currTemp')
 const form = document.querySelector('#form')
+const unit = document.querySelector('.unit')
 
 form.addEventListener('submit', async function(e) {
     e.preventDefault();
     const city = await form.elements.city.value;
     const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b73cddb55b44e62b4a4d236b57573959`)
-    temp.append(res.data.main.temp)
+    var currTemp = res.data.main.temp
+    const inCel = Math.round(currTemp - 273.15)
+    temp.innerHTML = `${inCel}`
+    unit.style.visibility = 'visible'
+    form.reset()
 })
 
 
-// const getWeatherInfo = async () => {
-//     try{
-//         const res = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=b73cddb55b44e62b4a4d236b57573959')
-//         temp.append(res.data.main.temp)
-        
-//     }
-//     catch(e) {
-//         console.log(e)
-//     }
-// }
